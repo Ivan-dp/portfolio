d99bc9283f17317404bf82ec44840ad421b42656
Иван Литовченко <ivan-dp@bk.ru>
2022-09-18 20:41:48 +0300

    modified card-about. Created drop description

--- src/components/CardAbout/CardAbout.jsx
+++ src/components/CardAbout/CardAbout.jsx
@@ -4,14 +4,17 @@ import { ReactComponent as GitHubLogo } from "./GitHub.svg";
 const CardAbout = () => {
   const [toggle, setToggle] = useState(true);
   const [className, setClassName] = useState("card-about");
+  const [className2, setClassName2] = useState("card-about__description");
 
   console.log(toggle);
 
   function classNameToggle() {
     if (toggle) {
-      setClassName("card-about_open");
+      setClassName("card-about open");
+      setClassName2("card-about__description open");
     } else {
       setClassName("card-about");
+      setClassName2("card-about__description");
     }
   }
 
@@ -33,13 +36,100 @@ const CardAbout = () => {
           </a>
         </div>
       </div>
-      <div className="card-about__description">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet maxime
-          similique numquam ducimus expedita veritatis non voluptatem ut
-          reiciendis modi debitis, atque harum enim autem nemo ex, saepe ipsam
-          ea.
-        </p>
+      <div className={className2}>
+        <div className="card-about__container">
+          <ul>
+            <li className="card-about__education">
+              <h2>Образование</h2>
+              <ul>
+                <li className="card-about__university">
+                  <h3>
+                    Московский финансово-промышленный университет "Синергия";
+                  </h3>
+                  <ul>
+                    <li className="card-about__specialty">
+                      <p>Информационные Системы и Технологии;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>
+                        Разработка, сопровождение и обеспечение безопасности
+                        информационных систем.
+                      </p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="card-about__university">
+                  <h3>БПЛ №17.</h3>
+                  <ul>
+                    <li className="card-about__specialty">
+                      <p>Технология Машиностроения, инженер-технолог.</p>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="card-about__container">
+          <ul>
+            <li className="card-about__education">
+              <h2>Дополнительное образование</h2>
+              <ul>
+                <li className="card-about__university">
+                  <h3>MetaLamp;</h3>
+                  <ul>
+                    <li className="card-about__specialty">
+                      <p>HTML/Pug;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>CSS/SCSS;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>JS;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>Webpack;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>JQuery.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="card-about__university">
+                  <h3>Стажировка у ментора;</h3>
+                  <ul>
+                    <li className="card-about__specialty">
+                      <p>React;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>Redux;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>React hooks;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>React.Context;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>JavaScript;</p>
+                    </li>
+                    <li className="card-about__specialty">
+                      <p>REST API.</p>
+                    </li>
+                  </ul>
+                </li>
+                <li className="card-about__university">
+                  <h3>FreeCodeCamp.</h3>
+                  <ul>
+                    <li className="card-about__specialty">
+                      <p>JavaScript.</p>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
